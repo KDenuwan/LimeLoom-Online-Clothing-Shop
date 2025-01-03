c131306138fbdea1b2af1c214f677bfa9946517b
KDenuwan <kavindudenuwan2002@email.com>
2025-01-03 13:14:18 +0530

    Project Structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <ModernNavbar />
+    {/* Rest of your app */}
+  </div>
+  );
+}
+
+export default App;
